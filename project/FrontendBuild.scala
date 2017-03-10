42dee377b99fa415a57a74fb8c9c74ceed2d90dc
Chris Waugh <chris@MacBook-Pro.local>
2017-03-10 09:20:09 +0000

    Get the project compiling with the new play-auth package
    Based changes on auth-demo-frontend

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -1,21 +1,25 @@
 import sbt._
-import play.sbt.PlayImport._
-import play.core.PlayVersion
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object FrontendBuild extends Build with MicroService {
+  import scala.util.Properties.envOrElse
 
   val appName = "lisa-frontend"
+  val appVersion = envOrElse("LISA_FRONTEND_VERSION", "999-SNAPSHOT")
 
-  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+
+  import play.sbt.PlayImport._
+  import play.core.PlayVersion
 
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "frontend-bootstrap" % "7.14.0",
     "uk.gov.hmrc" %% "play-partials" % "5.3.0",
     "uk.gov.hmrc" %% "play-authorised-frontend" % "6.3.0",
+    "uk.gov.hmrc" %% "play-auth" % "0.6.0",
     "uk.gov.hmrc" %% "play-config" % "4.2.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.1.0",
@@ -23,12 +27,13 @@ object FrontendBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "play-ui" % "7.0.0"
   )
 
-  def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
-    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
-    "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.8.1" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  val test = Seq(
+    "org.scalatest" %% "scalatest" % "2.2.6" % "test,it",
+    "org.pegdown" % "pegdown" % "1.4.2" % "test,it",
+    "org.jsoup" % "jsoup" % "1.7.3" % "test,it",
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % "test,it"
   )
+
+  def apply() = compile ++ test
 
 }
