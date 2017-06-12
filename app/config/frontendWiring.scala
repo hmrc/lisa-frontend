14d15f5fcef2bb0ecd215c3c31d3a5658754e543
raghuadimulam <raghu.adimulam@digital.hmrc.gov.uk>
2017-06-12 11:30:33 +0100

    [LISA-967] - audit config changes

--- app/config/frontendWiring.scala
+++ app/config/frontendWiring.scala
@@ -17,17 +17,19 @@
 package config
 
 import uk.gov.hmrc.auth.core.PlayAuthConnector
+import uk.gov.hmrc.play.audit.http.HttpAuditing
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
-import uk.gov.hmrc.play.audit.http.connector.{AuditConnector => Auditing}
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
 import uk.gov.hmrc.play.http.ws.{WSDelete, WSGet, WSPost, WSPut}
 
-object FrontendAuditConnector extends Auditing with AppName {
+object FrontendAuditConnector extends AuditConnector with AppName with RunMode {
   override lazy val auditingConfig = LoadAuditingConfig(s"auditing")
 }
 
-object WSHttp extends WSGet with WSPut with WSPost with WSDelete with AppName with RunMode {
-  override val hooks = NoneRequired
+object WSHttp extends WSGet with WSPut with WSPost with WSDelete with AppName with RunMode with HttpAuditing {
+  override val hooks = Seq(AuditingHook)
+  override val auditConnector = FrontendAuditConnector
 }
 
 object FrontendAuthConnector extends PlayAuthConnector with ServicesConfig {
