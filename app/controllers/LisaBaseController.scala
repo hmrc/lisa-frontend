1260f34ebad8ff54cbdcdaf92e662c325a3b835f
Chris Waugh <mail@chriswaugh.net>
2017-06-21 15:50:49 +0100

    WIP - refactoring auth and enrolment in base controller

--- app/controllers/LisaBaseController.scala
+++ app/controllers/LisaBaseController.scala
@@ -19,6 +19,7 @@ package controllers
 import config.FrontendAppConfig
 import connectors.UserDetailsConnector
 import models._
+import play.api.Logger
 import play.api.mvc.{AnyContent, Request, Result}
 import services.TaxEnrolmentService
 import uk.gov.hmrc.auth.core.AuthProvider.GovernmentGateway
@@ -27,8 +28,11 @@ import uk.gov.hmrc.auth.core._
 import uk.gov.hmrc.auth.frontend.Redirects
 import uk.gov.hmrc.http.cache.client.ShortLivedCache
 import uk.gov.hmrc.play.frontend.controller.FrontendController
+import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.Future
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
 trait LisaBaseController extends FrontendController
   with AuthorisedFunctions
@@ -39,43 +43,67 @@ trait LisaBaseController extends FrontendController
   val taxEnrolmentService:TaxEnrolmentService
 
   def authorisedForLisa(callback: (String) => Future[Result])(implicit request: Request[AnyContent]): Future[Result] = {
+    userStatus flatMap {
+      case UserNotLoggedIn => Future.successful(toGGLogin(FrontendAppConfig.loginCallback))
+      case UserUnauthorised => Future.successful(Redirect(routes.ErrorController.accessDenied()))
+      case user: UserAuthorised => {
+        getEnrolmentState(user.userDetails.groupIdentifier) flatMap {
+          case TaxEnrolmentPending => Future.successful(Redirect(routes.ApplicationSubmittedController.pending()))
+          case TaxEnrolmentError => Future.successful(Redirect(routes.ApplicationSubmittedController.rejected()))
+          case TaxEnrolmentSuccess => Future.successful(Redirect(routes.ApplicationSubmittedController.successful()))
+          case TaxEnrolmentDoesNotExist => callback(s"${user.internalId}-lisa-registration")
+        }
+      }
+    } recover {
+      case NonFatal(ex: Throwable) => {
+        Logger.warn(s"Auth error: ${ex.getMessage}")
+        Redirect(routes.ErrorController.error())
+      }
+    }
+  }
+
+  /* WIP - started refactoring auth & enrolment functionality with the aim of decoupling it from the base controller
+   * to make things more modular and to make testing & mocking a whole lot simpler */
+
+  trait LisaUserStatus
+  case object UserNotLoggedIn extends LisaUserStatus
+  case object UserUnauthorised extends LisaUserStatus
+  case class UserAuthorised(internalId: String, userDetails: UserDetails) extends LisaUserStatus
+
+  def userStatus(implicit hc:HeaderCarrier): Future[LisaUserStatus] = {
     authorised(
       AffinityGroup.Organisation and AuthProviders(GovernmentGateway)
     ).retrieve(internalId and userDetailsUri) { case (id ~ userUri) =>
       val userId = id.getOrElse(throw new RuntimeException("No internalId for logged in user"))
 
-      userUri match {
-        case Some(url) => {
-          userDetailsConnector.getUserDetails(url) flatMap { user =>
-            user.groupIdentifier match {
-              case Some(groupId) => {
-                taxEnrolmentService.getLisaSubscriptionState(groupId) flatMap {
-                  case TaxEnrolmentPending => Future.successful(Redirect(routes.ApplicationSubmittedController.pending()))
-                  case TaxEnrolmentError => Future.successful(Redirect(routes.ApplicationSubmittedController.rejected()))
-                  case TaxEnrolmentSuccess => Future.successful(Redirect(routes.ApplicationSubmittedController.successful()))
-                  case _ => callback(s"$userId-lisa-registration")
-                }
-              }
-              case None => {
-                Future.successful(Redirect(routes.ErrorController.error()))
-              }
-            }
-          }
-        }
-        case None => {
-          Future.successful(Redirect(routes.ErrorController.error()))
-        }
+      getUserDetails(userUri)(hc) map { user =>
+        UserAuthorised(userId, user)
       }
-    } recoverWith {
-      handleFailure
+    } recover {
+      case _ : NoActiveSession => UserNotLoggedIn
+      case _ : AuthorisationException => UserUnauthorised
     }
   }
 
-  def handleFailure(implicit request: Request[_]): PartialFunction[Throwable, Future[Result]] = PartialFunction[Throwable, Future[Result]] {
-    case _ : NoActiveSession => Future.successful(toGGLogin(FrontendAppConfig.loginCallback))
-    case _ : AuthorisationException => Future.successful(Redirect(routes.ErrorController.accessDenied()))
-    case _ => Future.successful(Redirect(routes.ErrorController.error()))
+  def getUserDetails(userDetailsUri: Option[String])(implicit hc:HeaderCarrier): Future[UserDetails] = {
+    userDetailsUri match {
+      case Some(url) => {
+        userDetailsConnector.getUserDetails(url)(hc)
+      }
+      case None => {
+        Future.failed(new RuntimeException("No userDetailsUri"))
+      }
+    }
   }
+
+  def getEnrolmentState(groupIdentifier: Option[String])(implicit hc:HeaderCarrier): Future[TaxEnrolmentState] = {
+    groupIdentifier match {
+      case Some(groupId) => taxEnrolmentService.getLisaSubscriptionState(groupId)
+      case None => Future.failed(new RuntimeException("Could not get groupIdentifier"))
+    }
+  }
+
+  /* ------------------------- regular service below ---------------------------- */
 
   def hasAllSubmissionData(cacheId: String)(callback: (LisaRegistration) => Future[Result])(implicit request: Request[AnyContent]): Future[Result] = {
     // get organisation details
