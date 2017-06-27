0c2bb3edd4630113de527aad3bceb9c17cc00833
Chris Waugh <mail@chriswaugh.net>
2017-06-27 15:44:33 +0100

    Fix for failing test

--- app/models/package.scala
+++ app/models/package.scala
@@ -20,8 +20,7 @@ import play.api.data.validation.{Valid, ValidationError, Invalid, Constraint}
 
 package object models extends Constants{
 
-  val companyPattern = pattern("""^[a-zA-Z0-9 '&\\/]{0,105}$""".r, error="Invalid Company name")
-  val utrPattern = pattern("""^[0-9]{10}$""".r, error="Invalid UTR")
+  val companyPattern: Constraint[String] = pattern("""^[a-zA-Z0-9 '&\\/]{0,105}$""".r, error="Invalid company name")
 
   def nonEmptyTextLisa[T](messageKey:String): Constraint[String] = Constraint[String](required) { text =>
     if (text == null) Invalid(messageKey) else if (text.trim.isEmpty) Invalid(ValidationError(messageKey)) else Valid
