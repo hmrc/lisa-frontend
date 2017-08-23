4c280f3ef25a5d4be95b695d9c62072930abca92
anand.prakash <anand.prakash@digital.hmrc.gov.uk>
2017-08-23 12:39:06 +0100

    Added the loggin for the Grafana and Kibana.

---
+++ app/metrics/EmailMetrics.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package metrics
+
+import java.util.concurrent.TimeUnit
+import com.codahale.metrics.MetricRegistry
+
+import uk.gov.hmrc.play.graphite.MicroserviceMetrics
+
+
+trait EmailMetrics {
+  def emailSentCounter(): Unit
+  def emailNotSentCounter(): Unit
+}
+
+object EmailMetrics extends EmailMetrics with MicroserviceMetrics {
+
+  val registry: MetricRegistry = metrics.defaultRegistry
+
+  override def emailNotSentCounter(): Unit = registry.counter("emailNotSentCounter").inc()
+
+  override def emailSentCounter(): Unit = registry.counter("emailSentCounter").inc()
+}
