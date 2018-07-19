633cc592d5d258a4f2e7be972e2207a927619adc
shravannallagoni <shravan.nallagoni@gmail.com>
2018-07-19 13:01:49 +0100

    Updated frontend bootstrap version to 8.24.0

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -16,7 +16,7 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.20.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.24.0",
     "uk.gov.hmrc" %% "play-partials" % "6.1.0",
     "uk.gov.hmrc" %% "auth-client" % "2.4.0",
     "uk.gov.hmrc" %% "http-caching-client" % "7.0.0"
