c26831a751b3bd89b90163d31365f3b84057ad9d
Chris Waugh <mail@chriswaugh.net>
2018-07-24 08:42:56 +0100

    Updated 500 error page

--- app/config/frontendGlobal.scala
+++ app/config/frontendGlobal.scala
@@ -19,6 +19,7 @@ package config
 import com.typesafe.config.Config
 import net.ceedubs.ficus.Ficus._
 import play.api.Play.current
+import play.api.i18n.Messages
 import play.api.i18n.Messages.Implicits._
 import play.api.mvc.Request
 import play.api.{Application, Configuration, Play}
@@ -27,7 +28,7 @@ import uk.gov.hmrc.crypto.ApplicationCrypto
 import uk.gov.hmrc.http.cache.client.{SessionCache, ShortLivedCache, ShortLivedHttpCaching}
 import uk.gov.hmrc.play.config.{AppName, ControllerConfig, RunMode, ServicesConfig}
 import uk.gov.hmrc.play.frontend.bootstrap.DefaultFrontendGlobal
-import uk.gov.hmrc.play.frontend.filters.{ FrontendAuditFilter, FrontendLoggingFilter, MicroserviceFilterSupport }
+import uk.gov.hmrc.play.frontend.filters.{FrontendAuditFilter, FrontendLoggingFilter, MicroserviceFilterSupport}
 
 object FrontendGlobal
   extends DefaultFrontendGlobal {
@@ -43,6 +44,15 @@ object FrontendGlobal
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: Request[_]): Html =
     views.html.error_template(pageTitle, heading, message)
+
+  override def internalServerErrorTemplate(implicit request: Request[_]): Html = {
+    val title = Messages("global.error.InternalServerError500.title")
+    val heading = Messages("global.error.InternalServerError500.heading")
+    val message = Messages("global.error.InternalServerError500.message")
+    val message2 = Messages("global.error.InternalServerError500.message2")
+
+    views.html.error_template(title, heading, message, Some(message2))
+  }
 
   override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"microservice.metrics")
 }
