a876ae7f92d36608d024e5f9f58556bd15bbdce1
RafalSobol <52891748+RafSobol@users.noreply.github.com>
2021-06-29 13:04:11 +0100

    [RS][DDCE-1776] Removing duplicate main element.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,17 +22,17 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.3.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.66.0-play-27",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.5.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.68.0-play-27",
     "uk.gov.hmrc" %% "play-partials" % "8.1.0-play-27",
     "uk.gov.hmrc" %% "http-caching-client" % "9.5.0-play-27",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.64.0-play-27"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.77.0-play-27"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.pegdown" % "pegdown" % "1.6.0",
     "com.typesafe.play" %% "play-test" % PlayVersion.current,
-    "org.mockito" % "mockito-core" % "3.10.0",
+    "org.mockito" % "mockito-core" % "3.11.2",
     "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3",
     "org.scalatestplus" %% "mockito-3-4" % "3.2.9.0"
   ).map(_ % Test)
