2fda43d411fff53bb02426b3b073b4b2a6979e64
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2023-01-23 11:25:27 +0000

    [DDCE-3295] play-frontend-hmrc to 6.2.0 (#243)
    * Copyright Years
    
    * play frontend hmrc to 6.2.0
    
    * Pr bot suggestions
    
    * scala style copyright header
    
    * enabled copyright year scalastyle and some warnings removed

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -25,7 +25,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0",
     "uk.gov.hmrc" %% "play-partials" % "8.2.0-play-28",
     "uk.gov.hmrc" %% "http-caching-client" % "9.5.0-play-28",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.4.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "6.2.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
