a88407efaf616bb4ee39c5aaf63e55591622a87e
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2024-06-06 10:35:51 +0100

    DDCE-5534: Play frontend upgrade (#254)
    * DDCE-5534: Play frontend upgrade
    
    * DDCE-5534: Dependency upgrade and PR bot feedback addressed
    
    * DDCE-5534: Reverting change as may not be applicable
    
    * DDCE-5534: Removed as per PR bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,15 +17,15 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "8.5.0"
-  val hmrcMongoPlayVersion = "1.8.0"
+  val bootstrapVersion = "8.6.0"
+  val hmrcMongoPlayVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"       % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"            % "9.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"               % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"       % "9.0.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"       % "9.11.0"
   )
 
   val test: Seq[ModuleID] = Seq(
