1fb7e7e850d59018467d0afd93e1066057720234
Shridhar Chippa <189346280+shridhar-chippa@users.noreply.github.com>
2025-06-03 17:17:55 +0100

    DDCE-2910: update ref number message for A11YT (#258)
    * DDCE-2910: update ref number message for A11YT
    
    * address pr bot comments
    
    * remove unused export file

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,7 +17,7 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.11.0"
+  val bootstrapVersion = "9.13.0"
   val hmrcMongoPlayVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -25,7 +25,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"       % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"            % "10.0.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"               % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"       % "9.11.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"       % "12.1.0"
   )
 
   val test: Seq[ModuleID] = Seq(
