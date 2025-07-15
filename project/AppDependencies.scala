89bc3bbcdd50b064a9088251a33c062a08b8b29d
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2025-07-15 11:15:49 +0100

    DDCE-6900: Bump bootstrapVersion

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "9.16.0"
   val hmrcMongoPlayVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -26,7 +26,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"       % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"            % "10.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"               % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"       % "12.6.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"       % "12.7.0"
   )
 
   val test: Seq[ModuleID] = Seq(
